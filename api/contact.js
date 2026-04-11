7e6e1b9447cab2a920cb3f702eebd22256aa0fbc
akiba <laurenzyepmeni005@gmail.com>
2026-04-11 19:25:02 +0200

    delete action

--- api/contact.js
+++ api/contact.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
         Name,
         Email,
         Phone : Phone || null,
-        Subject: subject || null,
+        
         Message: message,
       }),
     });
