69a3f8176622ad05bb13b303f87d5501bd4ac07b
akiba <laurenzyepmeni005@gmail.com>
2026-04-11 19:13:56 +0200

    modified

--- api/contact.js
+++ api/contact.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
         Name,
         Email,
         Phone : Phone || null,
-        subject: subject || null,
+        Subject: subject || null,
         Message: message,
       }),
     });
